77d1df3e97f05a4618521984a6dcf560dea0c38f
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2023-11-20 14:39:49 -0300

    Add type hints

--- src/keys.py
+++ src/keys.py
@@ -1,9 +1,18 @@
 import usb_hid
-from adafruit_hid.consumer_control import ConsumerControl
-from adafruit_hid.consumer_control_code import ConsumerControlCode
+from adafruit_hid.consumer_control import ConsumerControl as Media
+from adafruit_hid.consumer_control_code import ConsumerControlCode as MediaKey
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 from adafruit_hid.mouse import Mouse
+
+try:
+    from typing import TYPE_CHECKING
+except ImportError:
+    TYPE_CHECKING = False
+
+if TYPE_CHECKING:
+    Device = Keyboard | Mouse | Media
+    Codes = Keycode | Mouse | MediaKey
 
 
 def get_opts(cls: type[object]):
@@ -12,51 +21,45 @@ def get_opts(cls: type[object]):
     }
 
 
-sender_map = {}
+sender_map: dict[Device, tuple[type[Codes], set[str]]] = {}
 
 
 def init():
     global sender_map
     sender_map = {
-        Keyboard(usb_hid.devices): Keycode,
-        Mouse(usb_hid.devices): Mouse,
-        ConsumerControl(usb_hid.devices): ConsumerControlCode,
+        Keyboard(usb_hid.devices): (Keycode, get_opts(Keycode)),
+        Mouse(usb_hid.devices): (Mouse, get_opts(Mouse)),
+        Media(usb_hid.devices): (MediaKey, get_opts(MediaKey)),
     }
+    union_all = set()
+    for _, s in sender_map.values():
+        union_all |= s
+
+    assert sum(len(s) for _, s in sender_map.values()) == len(
+        union_all
+    ), "Overlapping constant names"
 
 
 class KeyWrapper:
-    __slot__ = ("key_names", "key_codes", "senders")
+    __slot__ = ("key_names", "params")
 
     def __init__(self, keys: str | list[str]) -> None:
+        global sender_map
         if isinstance(keys, str):
-            keys = [keys]
+            keys = [keys.upper()]
+        else:
+            keys = [key.upper() for key in keys]
 
-        key_sets = {
-            sender: get_opts(constants) for sender, constants in sender_map.items()
-        }
-        self._sanity_check(key_sets)
         self.key_names = keys
-        self.key_codes = []
-        self.senders = []
-        for key in keys:
-            key = key.upper()
-            for sender, key_set in key_sets.items():
-                if key in key_set:
-                    key_code = getattr(sender_map[sender], key)
-                    self.senders.append(sender)
-                    self.key_codes.append(key_code)
-
-    def _sanity_check(self, key_sets):
-        union_all = set()
-        for s in key_sets.values():
-            union_all |= s
-
-        assert sum(len(s) for s in key_sets.values()) == len(
-            union_all
-        ), "Overlapping constant names"
+        self.params = tuple(
+            (sender, getattr(key_container, key))
+            for sender, (key_container, key_set) in sender_map.items()
+            for key in keys
+            if key in key_set
+        )
 
     def _press(self, sender, key_code):
-        if isinstance(sender, ConsumerControl):
+        if isinstance(sender, Media):
             sender.send(key_code)
         elif isinstance(sender, Keyboard):
             sender.press(key_code)
@@ -64,7 +67,7 @@ class KeyWrapper:
             sender.click(key_code)
 
     def _release(self, sender, key_code):
-        if isinstance(sender, ConsumerControl):
+        if isinstance(sender, Media):
             pass
         elif isinstance(sender, Keyboard):
             sender.release(key_code)
@@ -72,11 +75,11 @@ class KeyWrapper:
             pass
 
     def press(self):
-        for sender, key_code in zip(self.senders, self.key_codes):
+        for sender, key_code in self.params:
             self._press(sender, key_code)
 
     def release(self):
-        for sender, key_code in zip(self.senders, self.key_codes):
+        for sender, key_code in self.params:
             self._release(sender, key_code)
 
     def press_and_release(self):
